4c7257520e9aa90e33b5e8beb636fe22cbf28ea1
Raleigh Littles III <raleighlittles@gmail.com>
2022-02-25 20:54:43 -0800

    fixed robot rodeo

--- robot_rodeo/test_robot_rodeo.py
+++ robot_rodeo/test_robot_rodeo.py
@@ -3,6 +3,12 @@ from robot_rodeo import robot_rodeo
 
 
 class TestDoesCircleExist(unittest.TestCase):
+
+    def test_sample_case(self):
+        robot_input = ["RG"]
+        expected_output = ["YES"]
+        self.assertEqual(robot_rodeo.doesCircleExist(robot_input), expected_output)
+
     def test_case_0(self):
         robot_input = ["G", "L"]
         expected_output = ["NO", "YES"]
