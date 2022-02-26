c0d4d2259e88becb54d84fe6386aa620fef5afa6
Raleigh Littles III <raleighlittles@gmail.com>
2022-02-25 20:00:28 -0800

    fixed algorithm swap...
    Was totally misinterpreting test case format. First digit is actually just the number of elements in the array, not part of the array itself

--- algorithm_swap/algorithm_swap.py
+++ algorithm_swap/algorithm_swap.py
@@ -6,23 +6,14 @@ def howManySwaps(arr):
     ## My approach
     ## Use two pointers, called j and k. Increment k until you find a pair of elements out of order. Once such a pair is found,
     ## do a swap, and move j up to where k was, and increment k by 1. Continue
-    j, k = 0, 0
+    j, k = 0, 1
     swaps = 0
 
-    while (j < len(arr)) and (k < len(arr)):
-        if arr[j] > arr[k]:
-            # You found elements out of order, do a swap.
-            swaps += 1
-            arr[j], arr[k] = arr[k], arr[j]
-            j, k = k, k + 1
-
-        else:
-            # Elements in correct order.
-            if (k < len(arr) - 2):
-                k += 1
-
-            else:
-                # You've reached the end of the list.
-                break
+    for j in range(0, len(arr)):
+        for k in range(j, len(arr)):
+            if arr[j] > arr[k]:
+                # elements are out of order
+                arr[j], arr[k] = arr[k], arr[j]
+                swaps += 1
 
     return swaps
